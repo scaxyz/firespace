a3b9accf6c1992f7b816665fe29118005d164cd6
Scax_yz <>
2023-03-10 07:21:17 +0100

    Add flags and args to cli

--- cli/firespace/main.go
+++ cli/firespace/main.go
@@ -2,34 +2,77 @@ package main
 
 import (
 	_ "embed"
+	"flag"
+	"os"
+	"path/filepath"
 
+	"github.com/adrg/xdg"
 	"github.com/scaxyz/firespace"
 	"go.uber.org/zap"
 )
 
-var logger, _ = zap.NewDevelopment()
-var sugar = logger.Sugar()
+var logger *zap.Logger
+var sugar *zap.SugaredLogger
+
+var configPath = flag.String("config", filepath.Join(xdg.ConfigHome, "firespace", "config.yaml"), "Path to the config file.")
+var dry = flag.Bool("dry", false, "Set run-dry mode.")
+var debug = flag.Bool("debug", false, "Enable debug logging.")
 
 func main() {
-	defer logger.Sync()
-	firespace.SetLogger(sugar)
+	defer func() {
+		logger.Sync()
+	}()
+	flag.Parse()
 
-	configFilePath := "../../test-validations.yml"
+	var err error
 
-	config, err := firespace.LoadYamlConfig(configFilePath)
-
-	if err != nil {
-		sugar.Fatalw("loading yaml config", zap.Error(err))
+	if *debug {
+		logger, err = zap.NewDevelopment(zap.IncreaseLevel(zap.DebugLevel))
+	} else if *dry {
+		logger, err = zap.NewDevelopment(zap.IncreaseLevel(zap.InfoLevel))
+	} else {
+		logger, err = zap.NewDevelopment(zap.IncreaseLevel(zap.WarnLevel))
 	}
 
-	space := firespace.NewFirespaceFromConfig(config, "user", "bat")
+	if err != nil {
+		panic(err)
+	}
+
+	sugar = logger.Sugar()
+
+	if len(flag.Args()) < 2 {
+		sugar.Error("please specify atleast a space to run")
+		os.Exit(1)
+	}
+
+	firespace.SetLogger(sugar)
+
+	config, err := firespace.LoadYamlConfig(*configPath)
+
+	if err != nil {
+		sugar.Panicw("loading yaml config", zap.Error(err), zap.String("config", *configPath))
+	}
+
+	if len(config.Spaces) == 0 {
+		sugar.Error("please create at leaset one space in your config")
+		os.Exit(1)
+	}
+
+	spaceName := flag.Arg(0)
+	programName := flag.Arg(1)
+
+	space := firespace.NewFirespaceFromConfig(config, spaceName, programName)
+
+	if space.Executeable == "" {
+		space.Executeable = programName
+	}
 
 	eSpace := space.ExecuteTemplates()
 
-	err = eSpace.Start([]string{"some-args-1", "some-args-2"}, true)
+	err = eSpace.Start(flag.Args()[2:], *dry)
 
 	if err != nil {
-		sugar.Errorw("runnig firejail", zap.Error(err))
+		sugar.Panicw("runnig firejail", zap.Error(err))
 	}
 
 }
